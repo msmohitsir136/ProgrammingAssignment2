3f403497fa9e7f2373717e33485e3070650ad01f
Mohit Singh <mohit@gameopedia.com>
2019-02-24 18:56:31 +0530

    matrix inverse caching feature is completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  if(nrow(x) == ncol(x)) {
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    
+  } else {
+    stop("Can not inverse it.")
+  }
+  
 
 }
 
@@ -12,4 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    inv
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv
 }
